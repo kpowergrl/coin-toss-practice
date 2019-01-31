efa7340adb9e7f7b7c223cd4c82de1a411ef7717
Kelly Power <kpowergrl@gmail.com>
2019-01-30 17:09:27 -0800

    mob build changes

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-/* eslint-disable */
-require = require('esm')(module);
-module.exports = require("./tests.js");
+const clickForm = document.getElementById('click-form');
+let image = document.getElementById('image');
+
+clickForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const randomNumber = Math.random();
+    if (randomNumber <= .5) {
+        console.log('brownKitten');
+        image.src = "https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=676&q=80";
+    } else {
+        console.log('grayKitten');
+        console.log(randomNumber);
+        image.alt = "new stuff for things!!!";
+        image.src = "https://www.catster.com/wp-content/uploads/2017/12/A-gray-kitten-meowing.jpg";
+    }
+
+
+});
+
---
+++ tests.js/tests.js
@@ -0,0 +1,5 @@
+const test = QUnit.test;
+
+test('', function() {
+
+});
